d6d373be77927ed6b58a8f9baa522fd7da8abb20
Mochammad Rizki <38099065+reztdev@users.noreply.github.com>
2022-07-31 23:03:47 +0700

    pyscan_nmap.py
    main

--- pyscanner.py
+++ pyscanner.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
-# ===========================================================
-# Written by Mochammad Rizki (ReztDev)
-# Required Python 3.x and modules nmap for run this tools
-# ===========================================================
 
 import sys
 if sys.version_info < (3, 0):
-    sys.stdout.write("Please required python 3.x for run this tools\n")
+    sys.stdout.write("Please required python 3.x for run this tools\nAnd install nmap tools.")
     sys.exit(0)
 import argparse
 import socket
@@ -15,7 +11,7 @@ import time
 try:
     import nmap
 except ImportError:
-    print("[!] Please install the modules NMAP!!")
+    print("[!] Please install the modules nmap!! (pip install python-nmap)")
     exit(0)
 
 class Color:
@@ -43,12 +39,12 @@ def config_scan(target_host, target_port):
         sys.exit(1)
 
 def main():
-    parser = argparse.ArgumentParser(prog=sys.argv[0], description="Port Scanner version 1.0",
+    parser = argparse.ArgumentParser(prog=sys.argv[0], description="Simple Port Scanner with Nmap version 1.0",
             usage="%(prog)s -s [HOST] -p [PORT <separated by comma>]",
             formatter_class=lambda prog: argparse.HelpFormatter(prog, max_help_position=75, width=80),
             epilog="Copyright (c) ReztDev 2018. All right reserved")
     parser.add_argument("-s", "--host", dest="hostname", help="specify target host")
-    parser.add_argument("-p", "--port", dest="port", help="specify target port[s] separated by comma")
+    parser.add_argument("-p", "--port", dest="port", help="specify target port[s] separated by comma (ex: 80,21,443)")
     options = parser.parse_args()
     port_scan = str(options.port).split(",")
     if (options.hostname == None) | (options.port == None):
@@ -56,15 +52,17 @@ def main():
         exit(0)
     else:
         start = time.time()
-        print("[{f}+{s}] Starting Port Scanner at {times}".format(
+        print("\n[{f}+{s}] Starting Port Scanner at {times}".format(
             f=Color.G, s=Color.W, times=time.strftime("%H:%M:%S-%p")
         ))
         for port_target in port_scan:
             config_scan(options.hostname, port_target)
         end = time.time()
-        print("[{f}+{s}] Completed Scanning. time elapsed: {total} seconds".format(
+        print("-" * 10)
+        print("[{f}+{s}] Time elapsed: {total} seconds".format(
             f=Color.G, s=Color.W, total=round(end-start)
         ))
+        print("[{f}+{s}] Scanning Completed.".format(f=Color.G, s=Color.W))
 
 if __name__ == '__main__':
     main()
